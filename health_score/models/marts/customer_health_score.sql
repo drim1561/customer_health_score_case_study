08f23416f3a3dd612026951e931a3e169646fe4d
Daniel Rim <danielrim18@gmail.com>
2026-05-21 13:51:46 +0000

    Try #2 Merge and CI job created

--- health_score/models/marts/customer_health_score.sql
+++ health_score/models/marts/customer_health_score.sql
@@ -12,8 +12,11 @@
 -- TIERS: Healthy >= 70 | At Risk 40-69 | Critical < 40
 with
 dimensions as (select * from {{ ref("int_account_dimensions") }}),
+
 metrics as (select * from {{ ref("int_period_metrics") }}),
+
 benchmarks as (select * from {{ ref("int_segment_benchmarks") }}),
+
 component_scores as (
 
     select
